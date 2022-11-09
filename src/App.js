2e7a217addd3a60c2621c759ce5eef54225b1fd8
Ewerton da Costa Vaz <ewerton.vaz@gmail.com>
2022-11-09 15:00:52 -0300

    interation 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import foods from './foods.json';
+import FoodBox from './components/FoodBox';
+import { Row, Divider, Button } from 'antd';
 
 function App() {
+  const [ allFoods, setFoods ] = useState( foods );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+        {
+          allFoods.map( (food, key) => {
+            return (
+              <FoodBox key={key} food={food} />
+            )
+          })
+        }
+      </Row>
     </div>
   );
 }
