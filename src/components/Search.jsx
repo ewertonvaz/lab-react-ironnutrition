283be59c2ac0086ac6681d1ee5e1cb742d228a3b
Ewerton da Costa Vaz <ewerton.vaz@gmail.com>
2022-11-10 10:09:00 -0300

    incluido um botão cencel search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,23 +1,34 @@
-import { Input, Divider } from 'antd';
+import { Input, Divider, Button } from 'antd';
 import { useState } from 'react';
 
 function Search({allFoods, setFoods}) {
-    const [search, setSearch] = useState();
+    const [search, setSearch] = useState("");
+    const [originalFoods] = useState([...allFoods]);
 
     function handleSearch(e){
+        e.preventDefault();
         setSearch(e.target.value);
         let filteredFoods = allFoods.filter( food => food.name.toLowerCase().includes(search.toLowerCase()) );
         setFoods(filteredFoods);
     }
 
+    function cancelSearch(e){
+        e.preventDefault();
+        setFoods(originalFoods);
+        setSearch("");
+    }
+
     return (
         <>
-        <Divider>Search</Divider>
-    
-        <label>Search</label>
-        <Input value={search} type="text" onChange={handleSearch} />
+            <Divider>Search</Divider>
+        
+            <label>Search</label>
+            <div style={{display: "flex", flexDirection:"row"}} >
+                <Input value={search} type="text" onChange={handleSearch} />
+                <Button type="primary" onClick={cancelSearch}>Cancel Search</Button>
+            </div>
         </>
-     );
+  );
 }
 
 export default Search;
